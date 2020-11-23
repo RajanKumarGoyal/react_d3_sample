f5cea4941e24fac14d823c6bf5cec0b9827f7e81
rajankumartpss <rajankumartpss@gmail.com>
2020-11-23 11:45:17 +0530

    Make Current Page Active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
                         <a className="navbar-brand" href="/#">ReactJs D3</a>
                     </div>  
                     <ul className="nav navbar-nav">
-                        <li><NavLink to={ "/" }>Home</NavLink></li>
-                        <li><NavLink to={ "/circle" }>Circle</NavLink></li>
-                        <li><NavLink to={ "/bar-chart" }>Bar Charts</NavLink></li>
-                        <li><NavLink to={ "/pie-chart" }>Pie Charts</NavLink></li>
+                        <li><NavLink to={ "/" } exact activeClassName="active-link">Home</NavLink></li>
+                        <li><NavLink to={ "/circle" } exact activeClassName="active-link">Circle</NavLink></li>
+                        <li><NavLink to={ "/bar-chart" } exact activeClassName="active-link">Bar Charts</NavLink></li>
+                        <li><NavLink to={ "/pie-chart" } exact activeClassName="active-link">Pie Charts</NavLink></li>
                     </ul>
                 </div>
             </nav>
